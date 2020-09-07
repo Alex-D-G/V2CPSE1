cd09c64c432b760353ec67f66245527be3d329f0
Alex-D-G <60549983+Alex-D-G@users.noreply.github.com>
2020-09-07 12:24:38 +0200

    Create print.asm

---
+++ Opdr4/print.asm
@@ -0,0 +1,29 @@
+.cpu cortex-m0
+.text
+.align 2
+.global print_asciz
+.global application
+.global convert_char
+
+asc:
+   .asciz "Hello world, the ANSWER is 42! @[]`{}~\n"
+
+print_asciz:
+   push { r5, lr }
+   mov  r5, r0
+loop: 
+   ldrb r0, [ r5 ]
+   add  r0, r0, #0
+   beq  done
+   bl   convert_char
+   bl   uart_put_char
+   add  r5, r5, #1
+   b    loop
+done: 
+   pop  { r5, pc }
+
+application:
+   ldr r0, =asc
+   push {lr}
+   bl   print_asciz
+   pop  {pc}
